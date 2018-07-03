ffd55169c0aae7abc1e2fb0140904c403c530f02
Admin <GA-Admin@Admins-MacBook-Pro-2.local>
2018-07-03 12:07:48 -0500

    working on collision but got muy obstical to move

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-console.log('we work')
+// console.log('we work')
+let counter = 0;
+let handle = 0
 
 // 1. make canvas 
 const canvas = document.getElementById('my-canvas')
-console.log(canvas.width);
-console.log(canvas.height);
+// console.log(canvas.width);
+// console.log(canvas.height);
 const ctx = canvas.getContext('2d')
 // step one done
 
@@ -15,44 +17,45 @@ class Player {
 	// player is a class, these are the properties for its location.
 		this.x = 125;
 		this.y = 120;
+		this.height = 20;
+		this.width = 30
 	}
 	// player has a method to draw itself 
 	paintPlayer(){
 		ctx.beginPath();
-		ctx.rect(this.x, this.y, 20, 30);
+		ctx.rect(this.x, this.y, this.height, this.width);
 		ctx.fillStyle = "#990000";
 		ctx.fill();
 		ctx.closePath();
 	}
 	// 3. player movement
 	movement(){
+
 	}
 }
-
-
-const game = {
-	// obstacles: [];
-
-	start() {
-		//create 4 obstacles
-			// instantiating a class 4 times and pushing into above array?
-	}
-};
 
 // 4. obstacle shapes -- stationary, based on data in an object.  Should you have an Obstacle class? (porobably so)
 class Obstacle {
 	constructor(){
 		this.x = 150;
 		this.y = 0;
+		this.height = 10
+		this.width = 10
 	}
-	paintOpstical(){
+	paintObstical(){
 		ctx.beginPath();
-		ctx.rect(this.x, this.y, 10, 10);
+		ctx.rect(this.x, this.y, this.height, this.width);
 		ctx.fillStyle = "black"
 		ctx.fill();
 		ctx.closePath();
 	}
+	move() { // this will get called every 1/60 of a seond
+		// this should chjange this obstacle's y value -- this f will be called by animateCanvas()
+		this.y++
+		// console.log("i'm moving") This was a check
+	}
 }
+
 
 const player1 = new Player();
 const firstObstacle = new Obstacle()
@@ -60,22 +63,34 @@ const firstObstacle = new Obstacle()
 document.addEventListener("keydown", (evt) => {
 	// up 38
   	if(evt.keyCode == 38 && player1.y > 0) {
-    	player1.y -= 10; // you may want to use a val much higher than 1
     	console.log("I'm up")
-
+		  
+    	player1.y -= 10; // you may want to use a val much higher than 1
   	}
 
   	// down 40
   	if(evt.keyCode == 40 && player1.y < 120) {
-  	  player1.y += 10; // you may want to use a val much higher than 1
   	  console.log("i'm down")
-  	  let counter = 0;
+  	  // function animateCanvas() {
+
+		  // any code here will be executed approx every 1/60th of a second
+		  // counter += 1000;
+		  // console.log("hey animate", counter);
+  	 	 player1.y += 10; // you may want to use a val much higher than 1
+
   	}
 
 		// left 37
   	if(evt.keyCode == 37 && player1.x > 0) {
-    	player1.x -= 10; // you may want to use a val much higher than 1
     	console.log("i'm left")
+    	// setInterval(function() {
+    	player1.x -= 10;
+  //   	if (player1.x === 0){
+  //   		stop();
+  //   	} // you may want to use a val much higher than 1
+		//   clearCanvas();
+		//   player1.paintPlayer();
+		// }, 1000/60);
   	}
 
 		// right 39
@@ -86,41 +101,78 @@ document.addEventListener("keydown", (evt) => {
 	// here every time I press a button it'll clear the canvas and recreate the player
 	clearCanvas();
 	player1.paintPlayer();
-	firstObstacle.paintOpstical();
+	firstObstacle.paintObstical();
 })
 // player1.movement();
-firstObstacle.paintOpstical();
+firstObstacle.paintObstical();
 player1.paintPlayer();
 
 function clearCanvas() {
   // this will erase the entire canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
+const game = {
+	// obstacles: [];
+
+	start() {
+		//create 4 obstacles
+			// instantiating a class 4 times and pushing into above array?
+	},
+	checkCollision() {
+		// using yesterday's discussion 
+		
+		const weHit = false
+		if (player1.x + player1.width >= firstObstacle.x && player1.x < firstObstacle.x + firstObstacle.width && player1.y + player1.height > firstObstacle.y && player1.y < firstObstacle.y + firstObstacle.height)  {
+			console.log("i'm working")
+			
+			return weHit = true
+		}
+		return weHit
+	}
+};
+
 
 	// and maybe move (= change location data)
 
  
 	// this should be methods on the player object
 
+// console.log(firstObstacle.x)
+// console.log(player1.width)
 
+console.log(player1.x + player1.width > firstObstacle.x)
+console.log(player1.x < firstObstacle.x + firstObstacle.width);
+console.log(player1.y + player1.height > firstObstacle.y);
+console.log(player1.y < firstObstacle.y + firstObstacle.height);
 
 // 3. player animation
-let counter = 0;
-function animateCanvas() {
+let theCanvas = canvas.width
+const animateCanvas = () => {
 
   // any code here will be executed approx every 1/60th of a second
   counter ++;
-  console.log("hey animate", counter);
-  
+  // console.log("hey animate", counter);
+  firstObstacle.move()
   clearCanvas();
   player1.paintPlayer();
+  // write a function in obstacle class that adjusts position of obstacle downward and call that function here
+  firstObstacle.paintObstical();
+
+  if(game.checkCollision === true) {
+  	alert("you've died")
+  	stop()
+  }
 
 
   // pass this function into w.rAF
-  window.requestAnimationFrame(animateCanvas)
+  handle = window.requestAnimationFrame(animateCanvas)
 }
 
  animateCanvas();
+
+const stop = () => {
+	window.cancelAnimationFrame(handle)
+}
 
 
 
